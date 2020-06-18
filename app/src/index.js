92d1d446879043e3b9cffb02691eb8000ae80b3a
cybertramp <paran_son@outlook.com>
2020-06-18 11:46:59 +0900

    시작하기 end

--- app/src/index.js
+++ app/src/index.js
@@ -1,19 +1,7 @@
 Vue.component('todo-item', {
   props: ['todo'],
-  template: '<li>{{todo.next}}</li>'
+  template: '<li>{{todo.text}}</li>'
 })
-
-var app7 = new Vue({
-  el: '#app-7',
-  data: {
-    groceryList: [
-      { id: 0, text: 'Vegetables' },
-      { id: 1, text: 'Cheese' },
-      { id: 2, text: 'Whatever else humans are supposed to eat' }
-    ]
-  }
-})
-
 
 var app = new Vue({
   el: '#app'
@@ -67,4 +55,13 @@ var app6 = new Vue({
   }
 })
 
-
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
