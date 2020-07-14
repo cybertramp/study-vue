abf5272df6defd1d15954b5dbae2accbfe695f88
paran_son <paran_son@outlook.com>
2020-07-14 09:49:41 +0900

    small fix

--- mp-vue/src/index.js
+++ mp-vue/src/index.js
@@ -3,3 +3,14 @@ import SuiVue from 'semantic-ui-vue';
 import $ from 'jquery';
 
 Vue.use(SuiVue);
+
+const getBreeds = async () => {
+    try {
+      return await axios.get('http://localhost:9000/Fileinfo/GetAllList');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+t=getBreeds()
+console.log(t)
