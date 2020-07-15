54bc9e8ae1aa5f5c66308565e1b29ef9d1f026a7
paran_son <paran_son@outlook.com>
2020-07-15 10:10:19 +0900

    latest

--- mp-vue/src/index.js
+++ mp-vue/src/index.js
@@ -1,16 +1,6 @@
-import Vue from 'vue';
-import SuiVue from 'semantic-ui-vue';
-import $ from 'jquery';
+//import Vue from 'vue';
+//import SuiVue from 'semantic-ui-vue';
+//import $ from 'jquery';
 
-Vue.use(SuiVue);
+//Vue.use(SuiVue);
 
-const getBreeds = async () => {
-    try {
-      return await axios.get('http://localhost:9000/Fileinfo/GetAllList');
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-t=getBreeds()
-console.log(t)
